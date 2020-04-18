33b0f40fc1d6a9d68599df06aec008190b143571
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-04-18 22:56:02 +0530

    http requests

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 <h1 align="center">Github Finder</h1>
 
+## CDN
+
+- Fontawesome: https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css
+
 ## Dependencies
+
+- axios: npm install --save axios
+
+## API
+
+- https://api.github.com/users
 
 ## Links
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -6,6 +7,12 @@ import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users`)
+      .then((res) => console.log(res.data));
+  }
+
   render() {
     return (
       <div className="App">
