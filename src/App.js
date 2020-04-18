bab238418e046466ee52ddd800f72d7d551a4302
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-04-18 13:50:03 +0530

    Expressions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  goo = () => 'Dar';
+
   render() {
+    const name = 'John';
+    const foo = () => 'Bar';
+
     return (
-      <Fragment>
-        <h1>Hello React</h1>
-        <label htmlFor="name">Name</label>
-      </Fragment>
+      <div className="App">
+        <h1>Hello {name.toUpperCase()}</h1>
+        <p>{2 + 2}</p>
+        <p>{foo()}</p>
+        <p>{this.goo()}</p>
+      </div>
     );
   }
 }
